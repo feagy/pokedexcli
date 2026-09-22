e70ccc09f62b387af8367f1ae4ea9d1d1088aae0
feagy <ferhat1903dnz@gmail.com>
2026-09-22 15:58:23 +0300

    help and exit commands added

--- main.go
+++ main.go
@@ -1,22 +1,8 @@
 package main
 
-import (
-	"fmt"
-    "bufio"
-    "os"
-)
 
 func main() {
-    scanner := bufio.NewScanner(os.Stdin)
-
-    for {
-        fmt.Print("Pokedex >")
-        if scanner.Scan() == true {
-            input_slice := cleanInput(scanner.Text())
-            fmt.Printf("Your command was: %v", input_slice[0])
-            fmt.Print("\n")
-        }
-    }
+    startREPL()
 }
 
 
--- repl.go
+++ repl.go
@@ -2,8 +2,66 @@ package main
 
 import (
     "strings"
+    "bufio"
+    "os"
+    "fmt"
 )
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands( )map[string]cliCommand{
+    return map[string]cliCommand{
+        "exit": {
+            name:        "exit",
+            description: "Exit the Pokedex",
+            callback:    commandExit,
+        },
+        "help": {
+            name:        "help",
+            description: "Displays a help message",
+            callback:    commandHelp,
+        },
+    }
+}
 
 func cleanInput(text string) []string {
     return strings.Fields(strings.TrimSpace(strings.ToLower(text)))
+}
+
+func startREPL(){
+    scanner := bufio.NewScanner(os.Stdin)
+    
+    for {
+        fmt.Print("Pokedex >")
+        if scanner.Scan() == true {
+            input_slice := cleanInput(scanner.Text())
+            command, ok := getCommands()[input_slice[0]]
+            if !ok {
+                fmt.Printf("Unknown command: %v", input_slice[0])
+            }
+            err := command.callback()
+            if err != nil {
+                fmt.Errorf("An erroe occured: %v", err)
+            }
+        }
+    }
+}
+
+func commandExit() error {
+    fmt.Printf("Closing the Pokedex... Goodbye!\n")
+    os.Exit(0)
+    return nil
+}
+
+func commandHelp() error {
+    fmt.Println("Welcome to the Pokedex!\n")
+    for _, v := range getCommands() {
+        fmt.Printf("Command: %v\n", v.name)
+        fmt.Printf("Description: %v\n\n", v.description)
+    }
+    return nil
 }
